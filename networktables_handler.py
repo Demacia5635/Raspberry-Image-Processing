b12aad4f1180d0ed8ff0984faefc864fc82bbd09
Yoav Nahumus <yoavnahum2005@gmail.com>
2022-01-03 19:51:49 +0000

    added the processing

--- networktables_handler.py
+++ networktables_handler.py
@@ -1,20 +1,25 @@
 from networktables import NetworkTables, NetworkTable
 from threading import Condition
 import requests
+import numpy as np
+import math
 
 robot_ip = '10.56.35.2'
 smart_dashboard : NetworkTable = None
 
 #TODO : put in default values
-min_hsv = (-1,-1,-1)
-max_hsv = (-1,-1,-1)
+min_hsv = np.array([20, 40, 80])
+max_hsv = np.array([30, 255, 255])
 
-camera_width = 1920
-camera_height = 1080
+aov = math.radians(50)
+ball_radius = 7.86 / 100 #cm
+
+camera_width = 640
+camera_height = 480
 camera_fps = 30
 
 def update_vars() -> None:
-    global camera_width, camera_height, camera_fps
+    global camera_width, camera_height, camera_fps, aov, ball_radius
 
     min_hsv[0] = smart_dashboard.getNumber('vision_min_h', min_hsv[0])
     min_hsv[1] = smart_dashboard.getNumber('vision_min_s', min_hsv[1])
@@ -23,6 +28,13 @@ def update_vars() -> None:
     max_hsv[0] = smart_dashboard.getNumber('vision_max_h', max_hsv[0])
     max_hsv[1] = smart_dashboard.getNumber('vision_max_s', max_hsv[1])
     max_hsv[2] = smart_dashboard.getNumber('vision_max_v', max_hsv[2])
+
+    aov = smart_dashboard.getNumber('vision_angle_of_view', aov)
+    ball_radius = smart_dashboard.getNumber('vision_angle_of_view', ball_radius)
+
+    camera_width = smart_dashboard.getNumber('vision_camera_width', camera_width)
+    camera_height = smart_dashboard.getNumber('vision_camera_height', camera_height)
+    camera_fps = smart_dashboard.getNumber('vision_camera_fps', camera_fps)
 
 def connect() -> None:
     global smart_dashboard
--- processing.py
+++ processing.py
@@ -1,11 +1,60 @@
 import cv2
 import numpy as np
 import networktables_handler
+import math
 
-def process_image(frame : np.ndarray) -> None:
-    #TODO : put in the processing algorithm (not in a loop)
-    pass
+def angle(px, x):
+    px_to_angle = networktables_handler.aov / px
+    return (px / 2 - x) * px_to_angle
+
+
+def distance(beta):  # in
+    return networktables_handler.ball_radius / math.sin(beta)
+
+def process_image(image : np.ndarray) -> None:
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(image, networktables_handler.min_hsv, networktables_handler.max_hsv)
+    kernal = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernal, 2)
+    mask = cv2.dilate(mask, kernal, 2)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) > 0:
+        for c in contours:
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            if radius > 10 and radius <= x < image.shape[1] - radius and radius <= y < image.shape[0] - radius:
+                radius /= 1.15
+                pixels = []
+                pixels.append(image[int(y - radius), int(x)])
+                pixels.append(image[int(y), int(x + radius)])
+                pixels.append(image[int(y), int(x - radius)])
+                #cv2.circle(oldimage, (int(x), int(y - radius)), 3, (0, 0, 0), 3)
+                #cv2.circle(oldimage, (int(x), int(y - radius)), 3, (0, 0, 0), 3)
+                #cv2.circle(oldimage, (int(x), int(y - radius)), 3, (0, 0, 0), 3)
+                draw = True
+                for pixel in pixels:
+                    if networktables_handler.min_hsv[0] > pixel[0] or pixel[0] > networktables_handler.max_hsv[0] or pixel[1] <= 50:
+                        draw = False
+                        break
+                if draw:
+                    radius *= 1.15
+                    #cv2.circle(oldimage, (int(x), int(y)), int(radius), (252, 40, 3), 3)
+
+                    beta = radius * networktables_handler.aov / image.shape[1]
+
+                    dis = distance(beta)
+
+                    alpha = ((image.shape[1] / 2) - x) * (math.degrees(networktables_handler.aov) / image.shape[1])
+
+                    output_vision(dis, alpha)
+                    #print("angle: " + str(alpha))
+                    #print("distance: " + str(dis))
+                #else:
+                    #radius *= 1.15
+                    #cv2.circle(oldimage, (int(x), int(y)), int(radius), (0, 0, 255), 3)
 
 def output_vision(distance : float, angle : float) -> None:
-    networktables_handler.smart_dashboard.putNumber('vision_distance', distance)
-    networktables_handler.smart_dashboard.putNumber('vision_angle', angle)
+    if (networktables_handler.smart_dashboard):
+        networktables_handler.smart_dashboard.putNumber('vision_distance', distance)
+        networktables_handler.smart_dashboard.putNumber('vision_angle', angle)
